863e620beb6244b7cbd794ce081453b01bfa6e20
Nataliia Martynyshyn <nataliya.map@gmail.com>
2026-04-29 21:14:05 +0300

    Add BaseComponent class with Selenium support

--- src/components/base_component.py
+++ src/components/base_component.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.support.ui import WebDriverWait
 
+
+class BaseComponent:
+
+    def __init__(self, driver, root):
+        self.driver = driver
+        self.root = root
+        self.wait = WebDriverWait(driver, 10)
+
+    def find(self, locator):
+        return self.root.find_element(*locator)
+
+    def click(self, locator):
+        self.root.find_element(*locator).click()
